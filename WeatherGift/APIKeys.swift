8a624756d7fa19e60081d260b981ede32b0e702f
Morgan Prime <primem@bc.edu>
2020-10-13 14:52:17 -0400

    6.10 done
    API keys not working yet

--- WeatherGift/APIKeys.swift
+++ WeatherGift/APIKeys.swift
@@ -10,4 +10,5 @@ import Foundation
 
 struct APIKeys{
     static let googlePlacesKey = "AIzaSyDbxmxmcLG-jyZ1j0yvdeA80B4Y_HmaZHw"
+    static let openWeatherKey = "948dbf4236566513916399cdb4283076"
 }
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -18,4 +18,36 @@ class WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
+    
+    func getData(){
+        let urlString = "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIKeys.openWeatherKey)"
+        print("Were accessing url \(urlString)")
+        
+        guard let url = URL(string: urlString) else{
+            print("Couldnt make url")
+            return
+        }
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url){(data, response, error) in
+            if let error = error{
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("yo \(json)")
+            }
+            catch{
+                print("Json error \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
 }
+
+
+//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=948dbf4236566513916399cdb4283076
