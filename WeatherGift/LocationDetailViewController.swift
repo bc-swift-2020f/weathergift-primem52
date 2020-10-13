bcdbd7f200bd665b0d7fe9968420c1aab00f72a6
Morgan Prime <primem@bc.edu>
2020-10-13 12:44:21 -0400

    6.7 done

---
+++ WeatherGift/LocationDetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LocationDetailViewController.swift
+//  WeatherGift
+//
+//  Created by Morgan Prime on 10/13/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var weatherLocation: WeatherLocation!
+    var weatherLocations: [WeatherLocation] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if weatherLocation == nil {
+            weatherLocation = WeatherLocation(name: "Current Location", latitude: 0.0, longitude: 0.0)
+            weatherLocations.append(weatherLocation)
+        }
+        loadLocations()
+        updateUserInterface()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func loadLocations(){
+        guard let locationsEncoded = UserDefaults.standard.value(forKey: "weatherLocations") as? Data
+            else {
+                print("error couldnt load data, unless this is first time onpening then its fine")
+                return
+        }
+        let decoder = JSONDecoder()
+        if let weatherLocations = try? decoder.decode(Array.self, from: locationsEncoded) as [WeatherLocation] {
+            self.weatherLocations = weatherLocations
+        }
+        else{
+            print("Couldnt decode data read from user defaults")
+        }
+    }
+    
+    func updateUserInterface(){
+        dateLabel.text = ""
+        placeLabel.text = weatherLocation.name
+        temperatureLabel.text = "--°"
+        summaryLabel.text = ""
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! LocationListViewController
+        destination.weatherLocations = weatherLocations
+    }
+    
+    @IBAction func unwindFromLocationListViewController(segue: UIStoryboardSegue){
+        let source = segue.source as! LocationListViewController
+        weatherLocations = source.weatherLocations
+        weatherLocation = weatherLocations[source.selectedLocationIndex]
+        updateUserInterface()
+        
+    }
+
+}
--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class LocationListViewController: UIViewController {
     
@@ -16,26 +17,52 @@ class LocationListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var weatherLocations: [WeatherLocation] = []
+    var selectedLocationIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Boston, MA", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Charlotte, NC", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
+//        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
+//        weatherLocation = WeatherLocation(name: "Boston, MA", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
+//        weatherLocation = WeatherLocation(name: "Charlotte, NC", latitude: 0, longitude: 0)
+//        weatherLocations.append(weatherLocation)
         
         
         tableView.dataSource = self
         tableView.delegate = self
     }
     
+    func saveLocations(){
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(weatherLocations){
+            UserDefaults.standard.set(encoded, forKey: "weatherLocations")
+        }
+        else{
+            print("error save encoding didnt work")
+        }
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        selectedLocationIndex = tableView.indexPathForSelectedRow!.row
+        saveLocations()
+    }
+    
+    
     
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        
+        //Display the autocomplete view controller
+        present(autocompleteController,animated: true, completion: nil)
     }
+    
+    
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
         if tableView.isEditing{
             tableView.setEditing(false, animated: true)
@@ -60,6 +87,7 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = weatherLocations[indexPath.row].name
+        cell.detailTextLabel?.text = "Lat: \(weatherLocations[indexPath.row].latitude) , Long: \(weatherLocations[indexPath.row].longitude)"
         return cell
     }
     
@@ -75,4 +103,37 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
         weatherLocations.insert(itemToMove, at: destinationIndexPath.row)
     }
     
+}
+
+extension LocationListViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    
+    let newLocation = WeatherLocation(name: place.name ?? "unknown place", latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+    weatherLocations.append(newLocation)
+    tableView.reloadData()
+    
+    dismiss(animated: true, completion: nil)
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+
+  // Turn the network activity indicator on and off again.
+  func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+  }
+
+  func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+  }
+
 }
