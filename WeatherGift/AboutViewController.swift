79d2da9620f8430604bdc893b921a8e506d601ec
Morgan Prime <primem@bc.edu>
2020-10-26 11:47:49 -0400

    6.22 done

---
+++ WeatherGift/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  WeatherGift
+//
+//  Created by Morgan Prime on 10/26/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
